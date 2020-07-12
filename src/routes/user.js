8363f4bbbfceaf307cdce50639007e0fb3a311cf
jerrenss <>
2020-07-12 16:25:28 +0800

    Added Validator

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 const { signUp } = require('../controllers/user')
+const { userSignupValidator } = require('../validators')
 
-router.post('/signup', signUp)
+router.post('/signup', userSignupValidator, signUp)
 
 module.exports = router
